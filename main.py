44f53fc2c970dec5ab56f3399e5e0b65a1a63d8f
tomspter <250176010@qq.com>
2021-04-06 22:29:39 +0800

    final push

--- main.py
+++ main.py
@@ -13,47 +13,74 @@ if __name__ == '__main__':
     data_times = index.xpath("//div[@class='history_news_content']//a/text()")
 
     for data_index, now_link in enumerate(data_links):
-        items_res = requests.get(now_link)
-        items_res.encoding = "utf-8"
-        items_html = etree.HTML(items_res.text)
-
-        if "2017" in data_times[data_index] or "2016" in data_times[data_index] or "2015" in data_times[
-            data_index] or "2014" in data_times[data_index]:
-            items_title = items_html.xpath("//div[@class='left_info']//a/text()")
-            items_links = items_html.xpath("//div[@class='left_info']//a/@href")
-        else:
+        if "2014" in data_times[data_index]:
+            items_res = requests.get(now_link)
+            items_res.encoding = "utf-8"
+            items_html = etree.HTML(items_res.text)
+            # if "2017" in data_times[data_index] or "2016" in data_times[data_index] or "2015" in data_times[
+            #     data_index] or "2014" in data_times[data_index]:
+            #     items_title = items_html.xpath("//div[@class='left_info']//a/text()")
+            #     items_links = items_html.xpath("//div[@class='left_info']//a/@href")
+            # elif "2017" in data_times[data_index]:
+            #     items_title = items_html.xpath("//ul[@class='news_list pb20']//a/text()")
+            #     items_links = items_html.xpath("//ul[@class='news_list pb20']//a/@href")
+            #     if len(items_title) == 0:
+            #         items_title = items_html.xpath("//div[@class='left_info']//a/text()")
+            #         items_links = items_html.xpath("//div[@class='left_info']//a/@href")
+            #         second_page = items_html.xpath("//div[@class='show_info_page']//a/@href")
+            #         if len(second_page) != 0:
+            #             items_res = requests.get(second_page[0])
+            #             items_res.encoding = "utf-8"
+            #             items_html = etree.HTML(items_res.text)
+            #             items_title = items_title + items_html.xpath("//div[@class='left_info']//a/text()")
+            #             items_links = items_links + items_html.xpath("//div[@class='left_info']//a/@href")
+            #
+            # else:
+            #     items_title = items_html.xpath("//ul[@class='news_list pb20']//a/text()")
+            #     items_links = items_html.xpath("//ul[@class='news_list pb20']//a/@href")
             items_title = items_html.xpath("//ul[@class='news_list pb20']//a/text()")
             items_links = items_html.xpath("//ul[@class='news_list pb20']//a/@href")
-        for index, title in enumerate(items_title):
-            for search in search_data:
-                if search in title:
-                    detail_res = requests.get(items_links[index])
-                    detail_res.encoding = "utf-8"
-                    detail_html = etree.HTML(detail_res.text)
-                    if "2014" in data_times[data_index]:
-                        money = detail_html.xpath("//div[@class='art_content']//tr[2]//td[2]/text()")
-                        if len(money) == 0:
-                            continue
-                        elif bool(re.search(r'\d', money[0])) == 0:
-                            money = detail_html.xpath("//tbody[@id='price_tbody']//tr[1]//td[3]/text()")
-                        if len(money) == 0:
-                            money = detail_html.xpath("//div[@class='art_content']//tbody//tr[1]//td[4]/text()")[0]
-                        else:
-                            money = re.findall(r'[1-9]\d*.\d*|0.\d*[1-9]\d*', money[0])[0]
-                        sheet.append((search, data_times[data_index], money.strip()))
-                    elif "2015" in data_times[data_index]:
-                        money = detail_html.xpath("//div[@class='art_content']//table//tr[4]//td[2]/text()")
-                        if len(money) == 0:
-                            money = detail_html.xpath("//div[@id='zoom']//table//tr[2]//td[2]//text()")
-                            if len(money) != 0:
-                                money = re.findall(r'[1-9]\d*.\d*|0.\d*[1-9]\d*', money[0])[0]
+            if len(items_title) == 0 and len(items_links) == 0:
+                items_title = items_html.xpath("//div[@class='left_info']//a/text()")
+                items_links = items_html.xpath("//div[@class='left_info']//a/@href")
+                second_page = items_html.xpath("//div[@class='show_info_page']//a/@href")
+                if len(second_page) != 0:
+                    print(second_page[0])
+                    items_res = requests.get(second_page[0])
+                    items_res.encoding = "utf-8"
+                    items_html = etree.HTML(items_res.text)
+                    items_title = items_title + items_html.xpath("//div[@class='left_info']//a/text()")
+                    items_links = items_links + items_html.xpath("//div[@class='left_info']//a/@href")
+            for index, title in enumerate(items_title):
+                for search in search_data:
+                    if search in title:
+                        detail_res = requests.get(items_links[index])
+                        detail_res.encoding = "utf-8"
+                        detail_html = etree.HTML(detail_res.text)
+                        if "2014" in data_times[data_index]:
+                            money = detail_html.xpath("//div[@class='art_content']//tr[2]//td[2]/text()")
+                            if len(money) == 0:
+                                continue
+                            elif bool(re.search(r'\d', money[0])) == 0:
+                                money = detail_html.xpath("//tbody[@id='price_tbody']//tr[1]//td[3]/text()")
+                            if len(money) == 0:
+                                money = detail_html.xpath("//div[@class='art_content']//tbody//tr[1]//td[4]/text()")[0]
                             else:
-                                money = 0
+                                money = re.findall(r'[1-9]\d*.\d*|0.\d*[1-9]\d*', money[0])[0]
+                            sheet.append((search, data_times[data_index], money.strip()))
+                        elif "2015" in data_times[data_index]:
+                            money = detail_html.xpath("//div[@class='art_content']//table//tr[4]//td[2]/text()")
+                            if len(money) == 0:
+                                money = detail_html.xpath("//div[@id='zoom']//table//tr[2]//td[2]//text()")
+                                if len(money) != 0:
+                                    money = re.findall(r'[1-9]\d*.\d*|0.\d*[1-9]\d*', money[0])[0]
+                                else:
+                                    money = 0
+                            else:
+                                money = money[0].strip()
+                            sheet.append((search, data_times[data_index], money))
                         else:
-                            money = money[0].strip()
-                        sheet.append((search, data_times[data_index], money))
-                    else:
-                        money = detail_html.xpath("//div[@class='art_content']//table//tr[4]//td[2]/text()")
-                        if len(money) != 0:
-                            sheet.append((search, data_times[data_index], money[0].strip()))
-    workbook.save("./seafoods_2018.xlsx")
+                            money = detail_html.xpath("//div[@class='art_content']//table//tr[4]//td[2]/text()")
+                            if len(money) != 0:
+                                sheet.append((search, data_times[data_index], money[0].strip()))
+    workbook.save("./excel/seafoods_2014.xlsx")
