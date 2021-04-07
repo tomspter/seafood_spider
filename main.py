9fb25f4d707d5bf0327dee6a52523000d3b5caf9
zhouhanbo <zhouhanbo@wisetv.com.cn>
2021-04-07 16:04:31 +0800

    push

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import openpyxl
 import requests
 from lxml import etree
 
-if __name__ == '__main__':
+def seafood_spider(year):
     search_data = ["对虾", "鲈鱼", "黑鱼", "鲤鱼", "草鱼"]
     workbook = openpyxl.Workbook()
     sheet = workbook.active
@@ -13,31 +13,10 @@ if __name__ == '__main__':
     data_times = index.xpath("//div[@class='history_news_content']//a/text()")
 
     for data_index, now_link in enumerate(data_links):
-        if "2014" in data_times[data_index]:
+        if str(year) in data_times[data_index]:
             items_res = requests.get(now_link)
             items_res.encoding = "utf-8"
             items_html = etree.HTML(items_res.text)
-            # if "2017" in data_times[data_index] or "2016" in data_times[data_index] or "2015" in data_times[
-            #     data_index] or "2014" in data_times[data_index]:
-            #     items_title = items_html.xpath("//div[@class='left_info']//a/text()")
-            #     items_links = items_html.xpath("//div[@class='left_info']//a/@href")
-            # elif "2017" in data_times[data_index]:
-            #     items_title = items_html.xpath("//ul[@class='news_list pb20']//a/text()")
-            #     items_links = items_html.xpath("//ul[@class='news_list pb20']//a/@href")
-            #     if len(items_title) == 0:
-            #         items_title = items_html.xpath("//div[@class='left_info']//a/text()")
-            #         items_links = items_html.xpath("//div[@class='left_info']//a/@href")
-            #         second_page = items_html.xpath("//div[@class='show_info_page']//a/@href")
-            #         if len(second_page) != 0:
-            #             items_res = requests.get(second_page[0])
-            #             items_res.encoding = "utf-8"
-            #             items_html = etree.HTML(items_res.text)
-            #             items_title = items_title + items_html.xpath("//div[@class='left_info']//a/text()")
-            #             items_links = items_links + items_html.xpath("//div[@class='left_info']//a/@href")
-            #
-            # else:
-            #     items_title = items_html.xpath("//ul[@class='news_list pb20']//a/text()")
-            #     items_links = items_html.xpath("//ul[@class='news_list pb20']//a/@href")
             items_title = items_html.xpath("//ul[@class='news_list pb20']//a/text()")
             items_links = items_html.xpath("//ul[@class='news_list pb20']//a/@href")
             if len(items_title) == 0 and len(items_links) == 0:
@@ -83,4 +62,7 @@ if __name__ == '__main__':
                             money = detail_html.xpath("//div[@class='art_content']//table//tr[4]//td[2]/text()")
                             if len(money) != 0:
                                 sheet.append((search, data_times[data_index], money[0].strip()))
-    workbook.save("./excel/seafoods_2014.xlsx")
+    workbook.save("./seafoods_{year}.xlsx".format(year=year))
+
+if __name__ == '__main__':
+    seafood_spider(2021)
